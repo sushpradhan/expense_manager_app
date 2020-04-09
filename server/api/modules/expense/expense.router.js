01ed8041f71eb77663c3e6e51b2bcb36c4dfe3bf
sushpr31 <sushpr31@in.ibm.com>
2020-04-09 15:12:42 +0530

    4 methods done

--- server/api/modules/expense/expense.router.js
+++ server/api/modules/expense/expense.router.js
@@ -2,5 +2,47 @@ const router = require('express').Router();
 
 // write all routing code and logic here
 
+var data = require('../../../db.json');
+
+router.get('/', (req, res)=>{
+
+    res.json(data);
+    res.end();
+})
+
+router.post('/', (req, res)=>{
+
+    const detail=req.body;
+    data.push(detail);
+    res.send("Details saved");
+    res.end();
+})
+
+router.delete('/:id', (req, res)=>{
+
+    const id= req.params.id;
+    const newData=data.filter(data => data.id !== id);
+    data=newData;
+    res.send("deleted");
+    res.end();
+})
+
+router.put('/:id', (req, res)=>{
+    const id= req.params.id;
+    const newexpense = req.body;
+    const newData=data.filter(data => data.id!==id);
+    data=newData;
+    data.push(newexpense);
+    res.send("updated");
+    res.end();
+
+})
+
+
+
+
+
+
+
 
 module.exports = router;
